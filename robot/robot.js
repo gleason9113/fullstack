f17e5997870b03fe9719ba938f94f938814db533
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-10-02 13:08:20 -0700

    Prettied robot file.

--- robot/robot.js
+++ robot/robot.js
@@ -5,11 +5,12 @@
  */
 
 //Points:  a, b, c, d, e, f, g, m, p, s, t
-//I changed the locations from the example code because I'm too lazy to type all that. 
+//I changed the locations from the example code because I'm too lazy to type all that.
 //Consider "p" to refer to the Post Office, however.
 
-const roads = [  //Array of edges
-  "a b", 
+const roads = [
+  //Array of edges
+  "a b",
   "a c",
   "a p",
   "b t",
@@ -20,8 +21,8 @@ const roads = [  //Array of edges
   "g s",
   "m f",
   "m p",
-  "m s"
-]
+  "m s",
+];
 
 const mailRoute = [
   "a",
@@ -36,7 +37,7 @@ const mailRoute = [
   "g",
   "f",
   "m",
-  "p"
+  "p",
 ];
 
 //Utility function for picking random element from array.
@@ -48,72 +49,72 @@ function randomPick(array) {
 //Utility function for finding a route from one point to another through the graph.
 //Todo:  Find a better algorithm?
 function findRoute(graph, from, to) {
-  let work = [{at: from, route: []}];
-  for(let i = 0; i < work.length; i++){
-    let {at, route} = work[i];
-    for(let place of graph[at]) {
-      if(place == to) return route.concat(place);
-      if(!work.some(w => w.at == place)) {
-        work.push({at: place, route: route.concat(place)});
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some((w) => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
       }
     }
   }
 }
 
 /**
- * 
+ *
  * @param {*} state A VillageState object.
  * @returns A randomly chosen direction from the options available at the current node.
  * This robot solves the problem by selecting nodes at random to travel to.  While this
  * is guaranteed to work, it represents the worst possible option in terms of efficiency.
  */
 function randomRobot(state) {
-  return {direction: randomPick(roadGraph[state.place])};
+  return { direction: randomPick(roadGraph[state.place]) };
 }
 
 /**
- * 
+ *
  * @param {*} state A VillageState object
  * @param {*} memory An array representing a route that passes through each node in the graph.
- * @returns The next node in the mailRoute array. 
- * This robot circles a specified route that visits each node in the graph.  This robot solves 
+ * @returns The next node in the mailRoute array.
+ * This robot circles a specified route that visits each node in the graph.  This robot solves
  * the problem in a maximum of 26 turns. Compare with the randomRobot above.
  */
 function routeRobot(state, memory) {
-  if(memory == undefined || memory.length == 0) {
+  if (memory == undefined || memory.length == 0) {
     memory = mailRoute;
   }
-  return {direction: memory[0], memory: memory.slice(1)};
+  return { direction: memory[0], memory: memory.slice(1) };
 }
 
 /**
- * 
+ *
  * @param {*} param0 The current location of the robot and the list of packages to be delivered.
  * @param {*} route The list of nodes to visit.
  * @returns The next node to visit and the updated route information
- * This robot uses the findRoute function to map its route based on the origin and destination nodes of the 
+ * This robot uses the findRoute function to map its route based on the origin and destination nodes of the
  * next package to be delivered.
  * Efficiency is better on average than the routeRobot (Run to compare).
  * The improvement here would be to find a better algorithm for generating a route.
  * Going further, more improvements can be made by sorting the packages in an order such that the number
  * of repeat visits to any node is minimized.
  */
-function searchRobot({place, parcels}, route) {
+function searchRobot({ place, parcels }, route) {
   if (route == undefined || route.length == 0) {
     let parcel = parcels[0];
-    if(parcel.place != place) {
+    if (parcel.place != place) {
       route = findRoute(roadGraph, place, parcel.place);
     } else {
       route = findRoute(roadGraph, place, parcel.address);
     }
   }
-  return {direction: route[0], memory: route.slice(1)};
+  return { direction: route[0], memory: route.slice(1) };
 }
 
 /**
- * 
+ *
  * @param {*} roads- Array of edges in format "a b"
- *  
+ *
  * @returns graph built from specified edges
  */
 function buildGraph(roads) {
@@ -126,19 +127,19 @@ function buildGraph(roads) {
       graph[from].push(to);
     }
   }
-  for (let [from, to] of roads.map(r => r.split(" "))) {
+  for (let [from, to] of roads.map((r) => r.split(" "))) {
     addEdge(from, to);
     addEdge(to, from);
   }
   return graph;
 }
 
-const roadGraph = buildGraph(roads);  //Now we have a graph built from the edges.
+const roadGraph = buildGraph(roads); //Now we have a graph built from the edges.
 console.log(roadGraph);
 
 /**
- * Key point from chapter:  It is not always necessary or desirable to turn everything into an object.  
- * Instead, consider the minimum set of values necessary to define this problem: the robot's position, the set of parcels, 
+ * Key point from chapter:  It is not always necessary or desirable to turn everything into an object.
+ * Instead, consider the minimum set of values necessary to define this problem: the robot's position, the set of parcels,
  * and the graph defining the relationships between the places.
  */
 class VillageState {
@@ -146,26 +147,32 @@ class VillageState {
     this.place = place;
     this.parcels = parcels;
   }
-  static random(parcelCount = 5) { //Method for generating a random starting VillageState
+  static random(parcelCount = 5) {
+    //Method for generating a random starting VillageState
     let parcels = [];
     for (let i = 0; i < parcelCount; i++) {
-      let address = randomPick(Object.keys(roadGraph)); 
+      let address = randomPick(Object.keys(roadGraph));
       let place;
       do {
         place = randomPick(Object.keys(roadGraph));
       } while (place == address); //Loop while origin matches destination
-      parcels.push({place, address});
+      parcels.push({ place, address });
     }
     return new VillageState("p", parcels);
   }
-  move(dest) { //Calculate a new VillageState based on a given move
-    if(!roadGraph[this.place].includes(dest)) { //The destination point is not directly reachable from the current location
+  move(dest) {
+    //Calculate a new VillageState based on a given move
+    if (!roadGraph[this.place].includes(dest)) {
+      //The destination point is not directly reachable from the current location
       return this;
     } else {
-      let parcels = this.parcels.map(p => {  //Gather each package that isn't addressed to the current location
-        if(p.place != this.place) return p; 
-        return {place: dest, address: p.address};
-      }).filter(p => p.place != p.address);  //Filter out packages that are addressed to the new location as well.
+      let parcels = this.parcels
+        .map((p) => {
+          //Gather each package that isn't addressed to the current location
+          if (p.place != this.place) return p;
+          return { place: dest, address: p.address };
+        })
+        .filter((p) => p.place != p.address); //Filter out packages that are addressed to the new location as well.
       return new VillageState(dest, parcels);
     }
   }
@@ -178,9 +185,9 @@ class VillageState {
  */
 
 function runRobot(state, robot, memory) {
-  
-  for (let turn = 0;; turn++) {
-    if(state.parcels.length == 0) { //All packages delivered
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      //All packages delivered
       console.log(`Done in ${turn} turns.`);
       break;
     }
@@ -195,7 +202,6 @@ let rndVillage = VillageState.random();
 runRobot(rndVillage, randomRobot);
 runRobot(rndVillage, routeRobot);
 runRobot(rndVillage, searchRobot);
-
 
 /**
  * My key takeaway:  Persistent data structures can be used to reduce code complexity.
