1e2b6f9740a5e8acc7a3820a5d9eea7de4949886
Ariel Gleason <gleas3@pdx.edu>
2022-10-06 15:39:18 -0700

    Revised coin and reverse scripts to more closely match assignment

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,24 +1,34 @@
 /** Exercise 01 - Coins **/
 
 const calculateChange = (input) => {
-  let total = input * 100;  //Convert dollar amount to cents.
-  let dollars, quarters, dimes, nickels, pennies; 
+  let total = input * 100; //Convert dollar amount to cents.
+  if (total > 1_000) {
+    return "Max value allowed is $10.00!";
+  }
+  let dollars, quarters, dimes, nickels, pennies;
   let drawer = [1, 5, 10, 25, 100];
   while (total > 0) {
-    let coin = drawer.pop();  //Get the next denomination of coin.
+    let coin = drawer.pop(); //Get the next denomination of coin.
     let amt = Math.floor(total / coin); //Get the # of coins of this denomination needed.
     total -= amt * coin; //Subtract value of coins from total.
-    switch (coin) {  //For formatting the results into a string literal.
-      case 100: dollars = amt;
-                break;
-      case 25:  quarters = amt;
-                break;
-      case 10:  dimes = amt;
-                break;
-      case 5:   nickels = amt;
-                break;
-      default:  pennies = amt;
-                break;          
+    switch (
+      coin //For formatting the results into a string literal.
+    ) {
+      case 100:
+        dollars = amt;
+        break;
+      case 25:
+        quarters = amt;
+        break;
+      case 10:
+        dimes = amt;
+        break;
+      case 5:
+        nickels = amt;
+        break;
+      default:
+        pennies = amt;
+        break;
     }
   }
   return `$${input} ==> ${dollars} dollars, ${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies.`;
--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,15 +1,16 @@
 /** Exercise 02 - Reverse **/
 
 /**
- * 
+ *
  * @param {*} input Value to be reversed
  * @returns Reverse of provided string
  * Input is validated for type and length, then reversed.
  */
 function reverseNum(input) {
-  if(typeof(input) !== "string" || input.length !== 8)
-    return "Invalid Input- Try Again"
-  return input.split('').reverse().join('');
+  if (typeof input !== "string") return "Error:  Invalid Input- Try Again";
+  else if (input.length !== 8)
+    return "Error:  Please enter a string containing 8 digits.";
+  return input.split("").reverse().join("");
 }
 
 /**
@@ -17,14 +18,19 @@ function reverseNum(input) {
  * Creates an element containing the reversed value and adds it to the HTML body.
  */
 function reverseMe() {
-  let value = reverseNum(document.getElementById("input").value);
+  let original = document.getElementById("input").value;
+  let value = reverseNum(original);
   let result = document.getElementById("resultbox");
-  if(result == undefined || result == null) {
+  console.log(result);
+  if (result == undefined || result == null) {
+    console.log("New resultbox created!")
     result = document.createElement("resultbox");
-    result.classList.add("text-white", "text-center");
+    result.id = "resultbox"
+    result.classList.add("text-center");
+    result.innerHTML = `<h6 class="mt-2 mb-4 mx-auto">${original} ==> ${value}</h1>`;
+    document.getElementById("main").appendChild(result);
+  } else {
+    result.innerHTML = `<h6 class="mt-2 mb-4 mx-auto">${original} ==> ${value}</h1>`;
   }
-  result.innerHTML = `<h1 class="mt-2 mb-4 mx-auto">${value}</h1>`;
-  document.body.appendChild(result); 
+  
 }
-
-
