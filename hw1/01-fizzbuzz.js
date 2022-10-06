04333e462fa50c49654de6a2be0ec087cd2b318a
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-10-06 14:17:06 -0700

    Update 01-fizzbuzz.js

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,9 +11,9 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   for (let i = 1; i < 101; i++) {
-    if (i % 3 === 0) console.log("fizz");  
+    if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+    else if (i % 3 === 0) console.log("fizz");  
     else if (i % 5 === 0) console.log("buzz");  
-    else if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
     else console.log(i);
   }
 };
