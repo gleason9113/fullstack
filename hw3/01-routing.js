1a533c0caaef3a801fcc3a16d7402fd139722b26
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-10-22 15:51:44 -0700

    Ex01 complete with Express, problems without.

--- hw3/01-routing.js
+++ hw3/01-routing.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const port = process.env.PORT || 5000;
+const express = require('express');
+const parser = require('cookie-parser');
+const routes = [
+  'welcome',
+  'redirect',
+  'redirected',
+  'cache',
+  'cookie',
+  'check-cookies',
+  'other',
+];
 
 // http://localhost:5000/welcome should return a status code 200 with a welcome message of your choice in html format
 
@@ -13,16 +24,16 @@ const port = process.env.PORT || 5000;
 
 // For other routes, such as http://localhost:5000/other, this exercise should return a status code 404 with '404 - page not found' in html format
 
-const server = http.createServer((req, res) => {
-  const routes = [
-    'welcome',
-    'redirect',
-    'redirected',
-    'cache',
-    'cookie',
-    'check-cookies',
-    'other',
-  ];
+
+  const app = express();
+  app.use(parser());
+
+  app.listen(port, (err) => {
+    if(err) {
+      throw(err);
+    }
+    console.log(`Listening on port ${port}...`);
+  });
 
   let getRoutes = () => {
     let result = '';
@@ -33,19 +44,139 @@ const server = http.createServer((req, res) => {
 
     return result;
   };
+  
+  app.get('/', (req, res) => {
+    let routeResults = getRoutes();
+      res.writeHead(200, { 'Content-Type': 'text/html' }, { 'shortcut-icon': '#'});
+      res.write(`<h1>Exercise 01</h1>`);
+      res.write(`<ul> ${routeResults} </ul>`);
+      res.end();
+  });
 
-  if (req.url === '/') {
+  app.get('/welcome', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<h1>Welcome!</h1>`);
+    res.end();
+  });
+
+  app.get('/redirect', (req, res) => {
+    res.writeHead(302, { 'Location': '/redirected' });
+    res.end();
+  });
+
+  app.get('/redirected', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<h1>You have been redirected!</h1>`);
+    res.write(`Unless you clicked the redirected link on the main page. Then you just travelled here.`);
+    res.end();
+  })
+
+  app.get('/cache', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`This resource was cached.`);
+    res.end();
+  });
+
+  app.get('/cookie', (req, res) => {
+    res.cookie("hello", "world");
+    res.write('cookies... yumm');
+    res.end();
+  });
+
+  app.get('/check-cookies', (req, res) => {
+    if (req.cookies == undefined) {
+      res.write('no');
+    } else {
+      res.write('yes');
+    }
+    res.end();
+  });
+
+  app.all('*', (req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/html'});
+    res.write(`<h1>404:  Whatever you're looking for, this ain't it.</h1>`);
+    res.end();
+  });
+  
+
+
+
+
+
+/*
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+if (req.url === '/') {
     let routeResults = getRoutes();
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<h1>Exercise 01</h1>`);
     res.write(`<ul> ${routeResults} </ul>`);
     res.end();
+  } else if (req.url === '/welcome') {
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.write(`<h1>Welcome!</h1>`);
   }
 
-  // Add your code here
-});
+  const server = http.createServer((req, res) => {
+  const routes = [
+    'welcome',
+    'redirect',
+    'redirected',
+    'cache',
+    'cookie',
+    'check-cookies',
+    'other',
+  ];
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  switch (req.url) {
+    case '/': 
+      let routeResults = getRoutes();
+      res.writeHead(200, { 'Content-Type': 'text/html' }, { 'shortcut-icon': '#'});
+
+      res.write(`<h1>Exercise 01</h1>`);
+      res.write(`<ul> ${routeResults} </ul>`);
+      res.end();
+      break;
+    case '/welcome':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<h1>Welcome!</h1>`);
+      res.end();
+      break;
+    case '/redirect':
+      res.writeHead(302, { 'Location': '/redirected' });
+      res.end();
+      break;
+    case '/cache':
+      res.writeHead(200, { 'Content-Type': 'text/html' }, { 'Cache-Control': 'max-age=1'});
+      res.write(`This resource was cached.`);
+      res.end();
+      break;
+    case '/cookie':
+      let cookie = {
+        "hello": "world",
+        "visited": "true",
+        "Max-Age": 600_000,
+      }
+      res.write('cookies... yumm');
+      res.end();
+      break;
+    case '/check-cookies':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      if (req.cookies == undefined) {
+        res.write('no');
+      } else {
+        res.write('yes');
+      }
+      res.end();
+      break;
+    default: 
+      res.writeHead(404, { 'Content-Type': 'text/html'});
+      res.write(`<h1>404:  Whatever you're looking for, this ain't it.</h1>`);
+      res.end();
+  };
+
+*/
---
+++ hw3/01-routing2.js
@@ -0,0 +1,80 @@
+const http = require('http');
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  const routes = [
+    'welcome',
+    'redirect',
+    'redirected',
+    'cache',
+    'cookie',
+    'check-cookies',
+    'other',
+  ];
+
+  let getRoutes = () => {
+    let result = '';
+
+    routes.forEach(
+      (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
+    );
+
+    return result;
+  };
+
+  switch (req.url) {
+    case '/':
+      let routeResults = getRoutes();
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<h1>Exercise 01</h1>`);
+      res.write(`<ul> ${routeResults} </ul>`);
+      res.end();
+      break;
+    case '/welcome':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<h1>Welcome!</h1>`);
+      res.end();
+      break;
+    case '/redirect':
+      res.writeHead(302, { 'Location': '/redirected' });
+      res.end();
+      break;
+    case '/redirected':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`<h1>You have been redirected!</h1>`);
+      res.write(`Unless you clicked the redirected link on the main page. Then you just travelled here.`);
+      res.end();
+      break;
+    case '/cache':
+      res.writeHead(200, { 'Content-Type': 'text/html' }, { 'Cache-Control': 'max-age: 86_400'});
+      res.write(`This resource was cached.`);
+      res.end();
+      break;
+    case '/cookie':
+      res.writeHead(200, { 'Content-Type': 'text/html' }, { 'Set-Cookie': 'hello=world; Max-Age=600_000; Same-Site=None'});
+      res.cookie
+      res.write('cookies... yumm');
+      res.end();
+      break;
+    case '/check-cookies':
+      if (req.cookies == undefined) {
+        res.write('no');
+      } else {
+        res.write('yes');
+      }
+      res.end();
+      break;
+    default: 
+      res.writeHead(404, { 'Content-Type': 'text/html'});
+      res.write(`<h1>404:  Whatever you're looking for, this ain't it.</h1>`);
+      res.end();
+  }
+  
+  
+
+  // Add your code here
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
