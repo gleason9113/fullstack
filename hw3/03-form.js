5e893bac6829bb1e23781ba10fd6273ab5145367
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-10-29 10:07:36 -0700

    Formatted form.js

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 5001;
 let fs = require("fs");
 
@@ -10,39 +10,39 @@ const server = http.createServer((req, res) => {
       (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
     );
     return result;
-  }
+  };
 
-  switch(req.url) {
-    case '/':
+  switch (req.url) {
+    case "/":
       res.writeHead(302, { Location: "/form" });
-      res.end(); 
+      res.end();
       break;
-    case '/form':
-      let form = fs.readFileSync('03-form.html');
+    case "/form":
+      let form = fs.readFileSync("03-form.html");
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(form);
       break;
-    case '/submit':
-      let body = '';
-      req.on('data', (data) => {
+    case "/submit":
+      let body = "";
+      req.on("data", (data) => {
         body += data;
       });
-      req.on('end', () => {
+      req.on("end", () => {
         let name;
         let email;
         let msg;
         let spam;
         let rawData = body.toString();
-        let fields = rawData.split('&');
+        let fields = rawData.split("&");
         console.log(fields);
-        name = `Name:  ` + fields[0].split('=')[1];
-        email = `Email:  ` + fields[1].split('=')[1];
-        if(fields[2] === 'message='){
+        name = `Name:  ` + fields[0].split("=")[1];
+        email = `Email:  ` + fields[1].split("=")[1];
+        if (fields[2] === "message=") {
           msg = `Comments:  n/a`;
         } else {
-          msg = `Comments:  ` + fields[2].split('=')[1];
+          msg = `Comments:  ` + fields[2].split("=")[1];
         }
-        if(fields.length === 4) {
+        if (fields.length === 4) {
           spam = `Newsletter:  Yes, I would like to sign up for the newsletter.`;
         } else {
           spam = `Newsletter: No, thank you.`;
@@ -53,8 +53,8 @@ const server = http.createServer((req, res) => {
         res.write(`<h4>${msg}</h4>`);
         res.write(`<h4>${spam}</h4>`);
         res.end();
-      })
-      
+      });
+
       break;
     default:
       res.writeHead(404, { "Content-Type": "text/html" });
@@ -63,12 +63,9 @@ const server = http.createServer((req, res) => {
   }
 });
 
-
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
 
