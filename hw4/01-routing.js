f37bb158981e688c5ea52e4e71acf735612c7852
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-10-29 14:18:13 -0700

    Ex01, Ex02, Ex03 complete.

--- hw4/01-routing.js
+++ hw4/01-routing.js
@@ -1,49 +1,107 @@
-const express = require('express');
-const app = express();
+const http = require("http");
 const port = process.env.PORT || 5000;
-
-// http://localhost:5000/welcome should return a status code 200 with a welcome message of your choice in html format
-
-// http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code / the redirected page should return a redirected message of your choice
-
-// http://localhost:5000/cache should return 'this resource was cached' in html format and set the cache max age to a day
-
-// http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
-
-// For other routes, such as http://localhost:5000/other, this exercise should return a status code 404 with '404 - page not found' in html format
-
+const express = require("express");
+const parser = require("cookie-parser"); //This is what I needed Express for.
 const routes = [
-  'welcome',
-  'redirect',
-  'redirected',
-  'cache',
-  'cookie',
-  'other',
+  "welcome",
+  "redirect",
+  "redirected",
+  "cache",
+  "cookie",
+  "check-cookies",
+  "other",
 ];
 
-let getRoutes = () => {
-  let result = '';
+const app = express(); //Set up the server.
+app.use(parser()); //Pass in cookie-parser.
 
+app.listen(port, (err) => {
+  //Server listening on ${port} (currently 5000).
+  if (err) {
+    console.log("Error setting up server:  " + err);
+    throw err;
+  }
+  console.log(`Listening on port ${port}...`);
+});
+
+let getRoutes = () => {
+  //Build a list of links for the root index.
+  let result = "";
   routes.forEach(
     (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
   );
-
   return result;
 };
 
-app.get('/', (req, res) => {
-  let routeResults = getRoutes();
+app.get("/", (req, res) => {
+  //The index route.
+  let routeResults = getRoutes(); //Get the list of routes.
+  res.writeHead(200, { "Content-Type": "text/html" }, { "shortcut-icon": "#" }); //Set header and status code.
+  res.write(`<h1>Exercise 01</h1>`); //Add html.
+  res.write(`<ul> ${routeResults} </ul>`); //More html.
+  res.end(); //Response complete.
+});
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Exercise 04</h1>`);
-  res.write(`<ul> ${routeResults} </ul>`);
+app.get("/welcome", (req, res) => {
+  //Welcome route- create basic html with welcome heading to display.
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h1>Welcome!</h1>`);
   res.end();
 });
 
-app.get('/welcome', (req, res) => {});
+app.get("/redirect", (req, res) => {
+  //Redirect route- reroute to /redirected.
+  res.writeHead(302, { Location: "/redirected" });
+  res.end();
+});
 
-// Add your code here
+app.get("/redirected", (req, res) => {
+  //Redirected route- Create landing page for redirection.
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h1>You have been redirected!</h1>`);
+  res.write(
+    `Unless you clicked the redirected link on the main page. Then you just travelled here.`
+  );
+  res.end();
+});
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get("/cache", (req, res) => {
+  //Cache route- Set cache max age to 1 day and print a message in plain text.
+  res.writeHead(
+    200,
+    { "Content-Type": "text/html" },
+    { "Cache-Control": "max-age: 86_400" }
+  );
+  res.write(`This resource was cached.`);
+  res.end();
+});
+
+app.get("/cookie", (req, res) => {
+  //Cookie route- Set a cookie with the values "hello=world". Add flavor text to website.
+  // res.write("cookies... yumm");
+  res.cookie(
+    "hello",
+    "world",
+    { expire: 600_000 + Date.now() },
+    { SameSite: "None" }
+  );
+
+  res.end();
+});
+
+app.get("/check-cookies", (req, res) => {
+  //Check-cookies route- Checks for cookie from /cookie and prints results on page.
+  if (req.cookies.hello) {
+    res.write(`yes`);
+  } else {
+    res.write(`no`);
+  }
+  res.end();
+});
+
+app.all("*", (req, res) => {
+  //404 error route- return a page displaying a custom error message.
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.write(`<h1>404:  Whatever you're looking for, this ain't it.</h1>`);
+  return res.end();
 });
--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,15 +1,102 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
+const memstore = require("memorystore")(session); //Recommended by express-session docs
 const app = express();
 const port = process.env.PORT || 5000;
+const currentRoute = `Currently on route:  `;
+const routes = ["spongebob", "patrick", "main", "sandy"];
+let getRoutes = () => {
+  //Build a list of links for the root index.
+  let result = "";
+  routes.forEach(
+    (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
+  );
+  return result;
+};
+let routeResults = getRoutes();
 
-// Add your code here
+let visited = (history) => {
+  //Build an html list from the cookie path and return it.
+  let list = ``;
+  let array = history.split(";");
+  array.forEach((elem) => (list += `<li><a href="/${elem}">${elem}</a></li>`));
+  return list;
+};
 
-// Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    //Setting up the Express session.
+    cookie: { maxAge: 14_400 },
+    store: new memstore({
+      checkPeriod: 14_400,
+    }),
+    secure: false,
+    resave: true,
+    secret: "the fnords",
+  })
+);
 
-app.get('/', (req, res) => {
-  // Add your code here
+app.get("/", (req, res) => {
+  // /main redirects to root.
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h2>Welcome to localhost:5000!</h2>`);
+  res.write(`<h3>${currentRoute}/</h3>`);
+  res.write(`Available Routes: `);
+  res.write(`<ul> ${routeResults} </ul>`);
+  req.session.cookie.path += `;/`;
+  res.write(`Previously visited: `);
+  res.write(`<ul> ${visited(req.session.cookie.path)} </ul>`);
+  res.end();
+});
+
+app.get("/spongebob", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h3>${currentRoute}/spongebob</h3>`);
+  res.write(`Available Routes: `);
+  res.write(`<ul> ${routeResults} </ul>`);
+  req.session.cookie.path += `;/spongebob`;
+  req.session.page_views++;
+  res.write(`Previously visited: `);
+  res.write(`<ul> ${visited(req.session.cookie.path)} </ul>`);
+  console.log(req.session);
+  res.end();
+});
+
+app.get("/main", (req, res) => {
+  res.writeHead(302, { Location: "/" });
+  res.end();
+});
+
+app.get("/patrick", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h3>${currentRoute}/patrick</h3>`);
+  res.write(`Available Routes: `);
+  res.write(`<ul> ${routeResults} </ul>`);
+  req.session.cookie.path += `;/patrick`;
+  res.write(`Previously visited: `);
+  res.write(`<ul> ${visited(req.session.cookie.path)} </ul>`);
+  res.end();
+});
+
+app.get("/sandy", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h3>${currentRoute}/sandy</h3>`);
+  res.write(`Available Routes: `);
+  res.write(`<ul> ${routeResults} </ul>`);
+  req.session.cookie.path += `;/sandy`;
+  res.write(`Previously visited: `);
+  res.write(`<ul> ${visited(req.session.cookie.path)} </ul>`);
+  res.end();
+});
+
+app.all("*", (req, res) => {
+  //404 error route- return a page displaying a custom error message.
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.write(`<h1>404:  Whatever you're looking for, this ain't it.</h1>`);
+  res.write(`Available Routes: `);
+  res.write(`<ul> ${routeResults} </ul>`);
+  req.session.cookie.path += `;/404error`;
+  return res.end();
 });
 
 app.listen(port, () => {
